939c155fff60aa42e22493c94cd033cf34ad140e
IT21180552 <it21180552@my.sliit.lk>
2023-12-10 21:54:00 +0530

    First Commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+	public class  AppUser
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public String UserName { get; set; }
+
+
+	}
+}
+
